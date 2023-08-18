1bf76e04282da81b0ee1a223a1c9ffc930c0fa6c
Arthur <ajeff9752@gmail.com>
2023-08-18 13:58:21 -0400

    lack of message saving notice on main.ejs

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 import expressSocketIO from 'express-socket.io-session'; // Import express-socket.io-session
 dotenv.config();
-//test commit
 
 const apiKey = process.env.API_KEY;
 const __filename = fileURLToPath(import.meta.url);
