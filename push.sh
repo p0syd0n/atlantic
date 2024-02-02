5f28717395b8487559a47280c02d233d64d692eb
p0syd0n <ajeff9752@gmail.com>
2024-02-02 02:07:06 +0000

    chore: push.sh

--- push.sh
+++ push.sh
@@ -1,17 +1,5 @@
 git add .
 git commit
-read -p 'major/minor/patch: ' type
-
-if [[ $type == 'major' ]]; then
- npm version major
-elif [[ $type == 'minor' ]]; then
- npm version minor
-elif [[ $type == 'patch' ]]; then
- npm version patch
-else 
- echo "Invalid input!"
- exit 1
-fi
-
+nvm install node
 npx semantic-release --no-ci
 git push
